36fdcb2fa0935c9c4aec2a272c49da974b40781b
John Doe <john.doe@gmail.com>
2019-07-10 10:22:36 -0300

    date time

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -11,7 +12,30 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("hello git");
+            string[] nameList = new string[2] { "valor 1","valor 2" };
+
+            foreach (var item in nameList)
+
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firtTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+
+                dateList[i] = DateTime.Now.ToString("yyyy:MM:dd:hh:mm:ss:f.fff");
+                Console.WriteLine(dateList[i]);
+            }
+
+            Console.WriteLine((DateTime.Now - firtTime).ToString());
+
+               
+            
+
+            
+                    Console.ReadKey();
+
 
         }
     }
